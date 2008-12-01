5ecfd4c3f76c339f272e73bce509b97e24d6c6c2
tfogal <tfogal@sci.utah.edu>
2008-12-01 18:32:34 +0000

    Made `nightly' play nicely with git, linux.
    This makes the nightly build script move the generated Linux
    binary to the top level directory, and tags on the revision number
    to the binary name.  Also adds a lot to handle using git svn,
    including the crappy hacked git-svn I need on tomasu..

--- nightly.sh
+++ nightly.sh
@@ -8,12 +8,39 @@ function try
         exit 1
     fi
 }
+if test -d .git ; then
+    if test -x `which tjf-git-svn`; then
+        svn="tjf-git-svn"
+    else
+        svn="git svn"
+    fi
+fi
+
+function update
+{
+    if test -d .git ; then
+        git diff --quiet
+        if test $? -ne 0 ; then
+            git stash save "hacks"
+            saved=1
+        fi
+        git checkout master
+        tjf-git-svn rebase
+        git checkout private
+        git rebase master
+        if test ${saved} -eq 1 ; then
+            git stash pop
+        fi
+    else
+        svn update
+    fi
+}
 
 spec="linux-g++"
 if test `uname` = "Darwin" ; then
     spec="macx-g++"
 fi
-svn update
+update
 
 # manual clean.
 find . \( -iname \*.o -or -iname moc_\*.cpp -or -iname ui_\*.h \) \
@@ -24,12 +51,15 @@ make clean
 make -j5
 try make
 
+revision=`$svn info | grep Revision | awk '{print $2}'`
+echo "revision: $revision"
 if test `uname` = "Darwin" ; then
 	echo "Building app file ..."
     try bash mk_app.sh
-	revision=`svn info | grep Revision | awk '{print $2}'`
 	pushd Build/OSX/Bin &>/dev/null
 	tar zcf iv3d-osx-r${revision}.tar.gz ImageVis3D.app
 	popd &>/dev/null
 	mv Build/OSX/Bin/iv3d-osx-r${revision}.tar.gz .
+elif test `uname` = "Linux" ; then
+    mv Build/Linux/Bin/ImageVis3D ./ImageVis3D-r${revision}
 fi
