e7897c405cc5eeeddc2ee5cf0dde132d69e46659
tfogal <tfogal@sci.utah.edu>
2008-12-19 17:50:14 +0000

    Simplify the script on the assumption of a static Qt.
    Don't copy the Qt frameworks into the app (shouldn't be needed
    anymore!), or do any of the install_name_tool hell.  Basically the
    app is just IV3D + the shaders, now.

--- mk_app.sh
+++ mk_app.sh
@@ -28,70 +28,12 @@ if ! test -d "${PREFIX}" ; then
     error "$PREFIX does not exist, build the application first!"
 fi
 
-mk_dir "${PREFIX}/Contents/Frameworks"
-
-for fw in QtCore.framework QtGui.framework QtOpenGL.framework ; do
-    echo "Removing ${PREFIX}/Contents/Frameworks/${fw}"
-    rm -fr "${PREFIX}/Contents/Frameworks/${fw}"
-done
-
-echo "Copying framework files ..."
-echo -e "\t Core"
-cp -R /Library/Frameworks/QtCore.framework "${PREFIX}/Contents/Frameworks"
-
-echo -e "\t GUI"
-cp -R /Library/Frameworks/QtGui.framework "${PREFIX}/Contents/Frameworks"
-
-echo -e "\t QtOpenGL"
-cp -R /Library/Frameworks/QtOpenGL.framework "${PREFIX}/Contents/Frameworks"
-
-echo -e "\t .. removing headers."
-pushd "${PREFIX}/Contents/Frameworks" &>/dev/null
-    for fw in QtCore QtGui QtOpenGL ; do
-        rm -r ${fw}.framework/Versions/4/Headers
-        rm ${fw}.framework/Headers
-    done
-popd &>/dev/null
-
 echo "Copying Shaders ..."
 rm -fr "${PREFIX}/Contents/Resources"
 mkdir -p "${PREFIX}/Contents/Resources"
 cp tuvok/Shaders/* "${PREFIX}/Contents/Resources"
 
-echo "Running install_name_tool"
-
-# FIXME These should probably be one for loop ..
-
-exit 0
-# set the identification names for the frameworks
-for fw in QtCore QtGui QtOpenGL ; do
-    install_name_tool -id \
-        @executable_path/../Frameworks/${fw}.framework/Versions/4/${fw} \
-        ${PREFIX}/Contents/Frameworks/${fw}.framework/Versions/4/${fw}
-    for sublib in QtGui QtOpenGL QtCore ; do
-        install_name_tool -change \
-            ${sublib}.framework/Versions/4/${sublib} \
-            @executable_path/../Frameworks/${sublib}.framework/Versions/4/${sublib} \
-            ${PREFIX}/Contents/Frameworks/${fw}.framework/Versions/4/${fw}
-    done
-done
-
-# change "pointers" to the frameworks
-for fw in QtCore QtGui QtOpenGL ; do
-    install_name_tool -change \
-        ${fw}.framework/Versions/4/${fw} \
-        @executable_path/../Frameworks/${fw}.framework/Versions/4/${fw} \
-        ${PREFIX}/Contents/MacOS/ImageVis3D
-done
-
-# change "pointers" within the frameworks cross referencing to core
-for fw in QtCore QtGui QtOpenGL ; do
-    install_name_tool -change \
-        ${fw}.framework/Versions/4/${fw} \
-        @executable_path/../Frameworks/${fw}.framework/Versions/4/${fw} \
-        ${PREFIX}/Contents/Frameworks/${fw}.framework/Versions/4/${fw}
-done
-
 echo -en "Removing subversion garbage ...\t"
 find "${PREFIX}" -iname .svn -exec rm -fr {} +
+
 echo "done!"
