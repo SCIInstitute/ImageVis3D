d61e2fdeba5ffe596427834a15a888f300cb9048
tfogal <tfogal@sci.utah.edu>
2009-01-29 19:30:50 +0000

    Fix help|about dialog text.
    The debug string was reversed.  I also simplified it since the
    version info was in both paths before, now it's simply appended.

--- ImageVis3D/UI/ImageVis3D_Help.cpp
+++ ImageVis3D/UI/ImageVis3D_Help.cpp
@@ -58,14 +58,25 @@ using namespace std;
 void MainWindow::ShowAbout()
 {
   QString qstrTitle;
-  QString qstrText;
+  QString qstrText("");
 #ifdef _DEBUG
-  qstrTitle = tr("ImageVis3D %1").arg(IV3D_VERSION);
-  qstrText =  tr("Warning this is a DEBUG build! This version is for testing only, some function run with dramatically reduced performance, please use a release build instead.\n\nThis is ImageVis3D %1 %2 it uses the Tuvok render engine %3 %4 %5.Copyrigth 2008 by the Scientific Computing and Imaging (SCI) Institute. Please report bugs to jens@sci.utah.edu").arg(IV3D_VERSION).arg(IV3D_VERSION_TYPE).arg(TUVOK_VERSION).arg(TUVOK_VERSION_TYPE).arg(TUVOK_DETAILS);
-#else
   qstrTitle = tr("ImageVis3D %1 %2 DEBUG VERSION!").arg(IV3D_VERSION).arg(IV3D_VERSION_TYPE);
-  qstrText =  tr("This is ImageVis3D %1 %2 it uses the Tuvok render engine %3 %4 %5.Copyrigth 2008 by the Scientific Computing and Imaging (SCI) Institute. Please report bugs to jens@sci.utah.edu").arg(IV3D_VERSION).arg(IV3D_VERSION_TYPE).arg(TUVOK_VERSION).arg(TUVOK_VERSION_TYPE).arg(TUVOK_DETAILS);
+  qstrText = tr("Warning: this is a DEBUG build!  This version is for "
+                "testing only.  Some functions may run with dramatically "
+                "reduced performance.  Please download a release build for "
+                "general use.\n\n");
+#else
+  qstrTitle = tr("ImageVis3D %1").arg(IV3D_VERSION);
 #endif
+  qstrText += tr("This is ImageVis3D %1 %2, using the Tuvok render engine "
+                 "%3 %4 %5.  Copyright 2008,2009 by the Scientific Computing "
+                 "and Imaging (SCI) Institute.  Please report bugs to "
+                 "iv3d-users@sci.utah.edu")
+                    .arg(IV3D_VERSION)
+                    .arg(IV3D_VERSION_TYPE)
+                    .arg(TUVOK_VERSION)
+                    .arg(TUVOK_VERSION_TYPE)
+                    .arg(TUVOK_DETAILS);
   QMessageBox::about(this, qstrTitle,qstrText);
 }
 
