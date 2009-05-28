9c1654e7bc9f372c4fccbb6f664e2db5917e520f
tfogal <tfogal@sci.utah.edu>
2009-05-28 01:05:24 +0000

    Use a constant manual URL on windows too.

--- Scripts/WinVM.bat
+++ Scripts/WinVM.bat
@@ -25,7 +25,7 @@ svn info > ..\rev2.txt
 cd ..
 pushd Scripts\installer
   del /f ImageVis3D.pdf
-  wget --no-check-certificate https://gforge.sci.utah.edu/gf/download/docmanfileversion/8/140/ImageVis3D.pdf
+  wget --no-check-certificate http://software.sci.utah.edu/releases/iv3d_docs/ImageVis3D.pdf
 popd
 
 
