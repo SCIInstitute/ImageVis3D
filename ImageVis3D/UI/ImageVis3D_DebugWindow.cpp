a568d37d87540fe5929c1263064640a6848a88dc
jens <jens@sci.utah.edu>
2009-09-09 13:26:25 +0000

    minor output cosmetic

--- ImageVis3D/UI/ImageVis3D_DebugWindow.cpp
+++ ImageVis3D/UI/ImageVis3D_DebugWindow.cpp
@@ -119,8 +119,8 @@ void MainWindow::ShowSysInfo() {
   UINT64 cpu_mem = mm->GetCPUMem();
   UINT64 max_cpu_mem = m_MasterController.SysInfo()->GetMaxUsableCPUMem();
 
-  sysinfo << "This is a " << m_MasterController.MemMan()->GetBitWithMem()
-          << "\nCPU Memory: Total " << cpu_mem / one_megabyte << " MB, "
+  sysinfo << "This is a " << m_MasterController.MemMan()->GetBitWithMem() << "bit build\n"
+          << "CPU Memory: Total " << cpu_mem / one_megabyte << " MB, "
           << "Usable " << max_cpu_mem << "MB\n"
           << "    Used: " << mm->GetAllocatedCPUMem()/one_megabyte << " MB "
           << "(" << mm->GetAllocatedCPUMem() << " bytes)\n";
