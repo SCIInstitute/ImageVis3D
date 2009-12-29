7a3f95640e90fd374a13ac611e0c3e6c69adb10a
tfogal <tfogal@sci.utah.edu>
2009-12-29 19:22:20 +0000

    Minor fix for Windows Qt build.
    Remove `rm' that was superfluous and only worked with a Cygwin
    install.  Fix `cp' to use non-Cygwin copy... oops.

--- Scripts/build-4.6.0.bat
+++ Scripts/build-4.6.0.bat
@@ -1,8 +1,7 @@
 set VER=4.6.0
-rm -fr qt-%VER%
 rmdir /S /Q qt-%VER% qt-%VER%-64bit
 "C:/program files/7-zip/7z.exe" x qt-everywhere-opensource-src-%VER%.zip
 move /Y qt-everywhere-opensource-src-%VER% qt-%VER%-64bit
 cd qt-%VER%-64bit
-cp ..\Win-StaticQt-%VER%.bat .
+copy /Y ..\Win-StaticQt-%VER%.bat .
 Win-StaticQt-%VER%.bat
