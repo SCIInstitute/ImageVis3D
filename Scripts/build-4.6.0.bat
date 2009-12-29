92d8f5dc82ec45b6e706253d4842beb6b002e8a4
tfogal <tfogal@sci.utah.edu>
2009-12-29 01:32:39 +0000

    (Hopefully) make the script work without Cygwin.

--- Scripts/build-4.6.0.bat
+++ Scripts/build-4.6.0.bat
@@ -1,7 +1,8 @@
 set VER=4.6.0
 rm -fr qt-%VER%
+rmdir /S /Q qt-%VER% qt-%VER%-64bit
 "C:/program files/7-zip/7z.exe" x qt-everywhere-opensource-src-%VER%.zip
-mv qt-everywhere-opensource-src-%VER% qt-%VER%-64bit
+move /Y qt-everywhere-opensource-src-%VER% qt-%VER%-64bit
 cd qt-%VER%-64bit
-cp ../Win-StaticQt-%VER%.bat ./
+cp ..\Win-StaticQt-%VER%.bat .
 Win-StaticQt-%VER%.bat
