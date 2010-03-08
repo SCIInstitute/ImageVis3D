281b1e653377b0096a6eee79996a424437e64fc5
tfogal <tfogal@sci.utah.edu>
2010-03-08 22:32:53 +0000

    Massive IO documentation update.
    *Finally* getting around to documenting UVF, file formats, how to
    hook into our IO subsystem, etc.

---
+++ doc/helium.py
@@ -0,0 +1,27 @@
+import os
+db = "/path/to/dataset.silo"
+OpenDatabase(db)
+
+AddPlot("Pseudocolor", "helium")
+AddOperator("Resample")
+ra = ResampleAttributes()
+ra.samplesX = 512
+ra.samplesY = 512
+ra.samplesZ = 512
+ra.distributedResample = False
+SetOperatorOptions(ra)
+
+DrawPlots()
+dbAtts = ExportDBAttributes()
+dbAtts.db_type = "BOV"
+dbAtts.filename = "singlehelium"
+dbAtts.variables = ("helium")
+ExportDatabase(dbAtts)
+
+RemoveAllOperators()
+DeleteAllPlots()
+
+CloseDatabase(db)
+
+import sys
+sys.exit(0)
