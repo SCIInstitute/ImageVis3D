97d8435ea506011f2ed73f4f63d07d077917cd82
tfogal <tfogal@sci.utah.edu>
2010-05-20 20:42:39 +0000

    Fix inline and debug settings.
    Apple broke debug mode on more recent compilers, so don't use that
    if we're on Mac.

--- Scripts/gcc-dev-cfg.sh
+++ Scripts/gcc-dev-cfg.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 #-D_REENTRANT -DQT_NO_DEBUG -DQT_GUI_LIB -DQT_CORE_LIB -DQT_SHARED
-VIS="-fvisibility-inlines-hidden -fvisibility=hidden"
+VIS="-fvisibility=hidden"
+INL="-fvisibility-inlines-hidden"
 CF="-g -Wall -Wextra -O0 -D_DEBUG"
-CXF="-D_GLIBCXX_DEBUG -D_GLIBCXX_CONCEPT_CHECK ${VIS} -Werror"
+CXF="-D_GLIBCXX_CONCEPT_CHECK -Werror"
+if test `uname -s` != "Darwin" ; then
+  CXF="${CXF} -D_GLIBCXX_DEBUG"
+fi
 if test -n "${QT_BIN}" ; then
     echo "Using custom qmake..."
     qm="${QT_BIN}/qmake"
@@ -14,8 +18,8 @@ for d in . ; do
   pushd ${d} &>/dev/null
     ${qm} \
         QMAKE_CONFIG="debug" \
-        QMAKE_CFLAGS="${CF}" \
-        QMAKE_CXXFLAGS="${CF} ${CXF}" \
+        QMAKE_CFLAGS="${VIS} ${CF}" \
+        QMAKE_CXXFLAGS="${VIS} ${INL} ${CF} ${CXF}" \
         -recursive
   popd &>/dev/null
 done
