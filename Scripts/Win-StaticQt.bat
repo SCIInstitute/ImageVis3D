5f8819a3f1b1fb272ffa6ee9972be1b2c5a63a74
tfogal <tfogal@sci.utah.edu>
2010-12-03 01:22:08 +0000

    Adjust vc proj files for Qt 4.7.

--- Scripts/Win-StaticQt.bat
+++ Scripts/Win-StaticQt.bat
@@ -1,2 +1,19 @@
-echo y | configure -static -qt-libtiff -qt-libpng -qt-libjpeg
+echo oy | configure ^
+	-static ^
+	-debug-and-release ^
+	-buildkey "imagevis3d" ^
+	-no-sql-sqlite ^
+	-no-sql-sqlite2 ^
+	-no-phonon ^
+	-no-scripttools ^
+	-no-webkit ^
+	-no-xmlpatterns ^
+	-no-multimedia ^
+	-no-qt3support ^
+	-nomake examples ^
+	-nomake demos ^
+	-nomake docs ^
+	-qt-libtiff ^
+	-qt-libpng ^
+	-qt-libjpeg
 nmake sub-src
