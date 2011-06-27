a1ae54cb345f4e21f7b773c0b65d6b63e3d669d5
tfogal <tfogal@sci.utah.edu>
2011-06-27 19:18:18 +0000

    Add script for configuring + building everything.
    This logic is in our buildbot scripts at present.  I finally agree
    with other buildbot users that embedding smarts into your repo and
    keeping buildbot scripts dumb is the better way to go.

---
+++ Scripts/oneshot-debug.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# Script to build everything we can in a single invocation, using
+# a set of options which is appropriate for creating debug builds.
+
+VIS="-fvisibility=hidden"
+INL="-fvisibility-inlines-hidden"
+COVERAGE="-fprofile-arcs -ftest-coverage"
+CF="-g -Wall -Wextra -O0 -D_DEBUG ${COVERAGE}"
+CXF="-D_GLIBCXX_CONCEPT_CHECK -Werror ${COVERAGE}"
+LDFLAGS="${COVERAGE}"
+# Darwin's debug STL support is broken.
+if test `uname -s` != "Darwin"; then
+  CXF="${CXF} -D_GLIBCXX_DEBUG"
+fi
+
+# Users can set the QT_BIN env var to point at a different Qt implementation.
+if test -n "${QT_BIN}" ; then
+  echo "Using qmake from '${QT_BIN}' instead of the default from PATH."
+  qm="${QT_BIN}/qmake"
+else
+  qm="qmake"
+fi
+
+echo "Configuring..."
+for d in . Tuvok/IO/test ; do
+  pushd ${d} &> /dev/null || exit 1
+    ${qm} \
+      QMAKE_CONFIG+="debug" \
+      QMAKE_CFLAGS+="${VIS} ${CF}" \
+      QMAKE_CXXFLAGS+="${VIS} ${INL} ${CF} ${CXF}" \
+      QMAKE_LFLAGS+="${VIS} ${COVERAGE}" \
+      -recursive || exit 1
+  popd &> /dev/null
+done
+
+# Unless the user gave us input as to options to use, default to a small-scale
+# parallel build.
+if test -z "${MAKE_OPTIONS}" ; then
+  MAKE_OPTIONS="-j2 -l 2.0"
+fi
+
+for d in . Tuvok/IO/test ; do
+  echo "BUILDING IN ${d}"
+  pushd ${d} &> /dev/null || exit 1
+    make --no-print-directory ${MAKE_OPTIONS}
+  popd &> /dev/null
+done
+
+echo "Bundling..."
+if test `uname -s` = "Darwin" ; then
+  bash Scripts/mk_app.sh
+else
+  bash Scripts/mk_tarball.sh
+fi
