720bec33f532d42c56c81dae95971a5a9126fa91
jhughes <jhughes@sci.utah.edu>
2012-06-12 18:05:23 +0000

    Changing to msbuild instead of VCExpress.exe.

--- Scripts/oneshot-debug.bat
+++ Scripts/oneshot-debug.bat
@@ -2,16 +2,21 @@ REM Script to build everything we can in a single invocation, using
 REM a set of options which is appropriate for creating debug builds.
 
 set w32_cf="-D_CRT_SECURE_NO_WARNINGS=1 -D_SCL_SECURE_NO_WARNINGS=1"
-qmake -tp vc ^
-  QMAKE_CFLAGS+=%w32_cf% ^
-  QMAKE_CXXFLAGS+=%w32_cf% ^
-  -recursive ^
-  ImageVis3d.pro
+REM qmake -tp vc ^
+REM  QMAKE_CFLAGS+=%w32_cf% ^
+REM  QMAKE_CXXFLAGS+=%w32_cf% ^
+REM  -recursive ^
+REM  ImageVis3d.pro
 REM hardcoding vs2010 express for now =(
-set bld="C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\VCExpress.exe"
+REM set bld="C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\VCExpress.exe"
+REM TODO - Set visual studio 2010 environment variables.
+REM Set environment for x86 build tools.
+"C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+set bld="msbuild"
 %bld% ^
   ImageVis3D-2010.sln ^
-  /build Release
+  /nologo ^
+  /t:Rebuild
 
 pushd Tuvok\IO\test
   python ../3rdParty/cxxtest/cxxtestgen.py ^
