92182bdb1edd6d946834781489447741d7f90f5c
jhughes <jhughes@sci.utah.edu>
2012-06-26 22:55:59 +0000

    CI 64-bit build support for windows.

--- Scripts/oneshot-debug.bat
+++ Scripts/oneshot-debug.bat
@@ -1,6 +1,15 @@
 REM Script to build everything we can in a single invocation, using
 REM a set of options which is appropriate for creating debug builds.
 
+REM Is there better flow control than GOTO statements on windows?
+REM IF statements appear to be 1 line only.
+IF "%1"=="" GOTO x32_1
+REM Handle 64 bit compilation
+GOTO CompilationFinish
+:X32_1
+
+:CompilationFinish
+
 set w32_cf="-D_CRT_SECURE_NO_WARNINGS=1 -D_SCL_SECURE_NO_WARNINGS=1"
 REM qmake -tp vc ^
 REM  QMAKE_CFLAGS+=%w32_cf% ^
@@ -9,14 +18,25 @@ REM  -recursive ^
 REM  ImageVis3d.pro
 REM hardcoding vs2010 express for now =(
 REM set bld="C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\VCExpress.exe"
-REM TODO - Set visual studio 2010 environment variables.
-REM Set environment for x86 build tools.
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
 set bld="msbuild"
-%bld% ^
-  ImageVis3D-2010.sln ^
-  /nologo ^
-  /t:Rebuild
+IF "%1"=="x64" (
+  REM Handle 64 bit compilation
+  call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x64
+  %bld% ^
+    ImageVis3D-2010.sln ^
+    /nologo ^
+    /p:Configuration="Debug",Platform=x64 ^
+    /t:Rebuild
+) ELSE (
+  REM Handle 32 bit compilation
+  call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+  %bld% ^
+    ImageVis3D-2010.sln ^
+    /nologo ^
+    /p:Configuration="Debug",Platform=Win32 ^
+    /t:Rebuild
+)
 
 pushd Tuvok\IO\test
   python ../3rdParty/cxxtest/cxxtestgen.py ^
--- Scripts/oneshot-release.bat
+++ Scripts/oneshot-release.bat
@@ -1,14 +1,25 @@
 REM IV3D release build
 
 set w32_cf="-D_CRT_SECURE_NO_WARNINGS=1 -D_SCL_SECURE_NO_WARNINGS=1"
-REM Set environment for x86 build tools (need to check x86_64)  
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
 set bld="msbuild"
-%bld% ^
-  ImageVis3D-2010.sln ^
-  /nologo ^
-  /p:Configuration=Release ^
-  /t:Rebuild
+IF "%1"=="x64" (
+  REM Handle 64 bit compilation
+  call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x64
+  %bld% ^
+    ImageVis3D-2010.sln ^
+    /nologo ^
+    /p:Configuration="Release",Platform=x64 ^
+    /t:Rebuild
+) ELSE (
+  REM Handle 32 bit compilation
+  call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+  %bld% ^
+    ImageVis3D-2010.sln ^
+    /nologo ^
+    /p:Configuration="Release",Platform=Win32 ^
+    /t:Rebuild
+)
 
 pushd Tuvok\IO\test
   python ../3rdParty/cxxtest/cxxtestgen.py ^
