2b0f2a5f09fd139b201bb0de75e2b5ed655cd49c
Tom Fogal <tfogal@alumni.unh.edu>
2014-04-10 16:43:15 +0200

    half-fix some scripting issues creating Linux binaries.

--- Scripts/util.sh
+++ Scripts/util.sh
@@ -46,18 +46,15 @@ function mailtry
 # Determines the appropriate VCS system to use.  Mainly to handles/distinguish
 # between svn and git-svn repositories.  Sets variable `VCS' to the appropriate
 # executable.
+# We switched to github and thus always git in 2014.
 VCS=""
 function _vcs
 {
-    if test -d .git ; then
-        VCS="git svn"
-    else
-       VCS="svn"
-    fi
+    VCS="git"
 }
 
 # Updates a git-svn repository.  One argument: a directory to cd to first.
-function git_svn_update
+function git_rebase
 {
     pushd "$@"
     git diff --exit-code --quiet &>/dev/null
@@ -67,7 +64,7 @@ function git_svn_update
         saved=1
     fi
     git checkout master
-    $VCS rebase
+    git pull --rebase
     git checkout private
     git rebase master
     if test $saved -eq 1 ; then
@@ -87,31 +84,19 @@ function vcs_update
     if test "x${VCS}" = "xsvn" ; then
         svn update
     else
-        git_svn_update "."
-        git_svn_update "Tuvok"
-        git_svn_update "Tuvok/Basics"
-        git_svn_update "Tuvok/IO"
+        git_rebase "."
     fi
 }
 
 # Echoes the revision numbers from the two repositories.  Also sets
-# R_IMAGEVIS3D and R_TUVOK shell variables.
+# R_IMAGEVIS3D shell variable.
 function revision
 {
     if test -z "${VCS}" ; then
         _vcs
     fi
-    R_IMAGEVIS3D=`$VCS info | grep Revision | awk '{print $2}'`
-    pushd Tuvok &>/dev/null
-        R_TUVOK=`$VCS info | grep Revision | awk '{print $2}'`
-    popd &> /dev/null
-    pushd Tuvok/Basics &>/dev/null
-        R_BASICS=`$VCS info | grep Revision | awk '{print $2}'`
-    popd &> /dev/null
-    pushd Tuvok/IO &>/dev/null
-        R_IO=`$VCS info | grep Revision | awk '{print $2}'`
-    popd &> /dev/null
-    echo "${R_IMAGEVIS3D}_${R_TUVOK}_${R_BASICS}_${R_IO}"
+    R_IMAGEVIS3D=`git log --stat --pretty=one HEAD^..HEAD | head -n 1 | awk '{print $1}'`
+    echo "${R_IMAGEVIS3D}"
 }
 
 # Reads the version numbers from Std*Defines.h.  Sets IV3D_VERSION,
