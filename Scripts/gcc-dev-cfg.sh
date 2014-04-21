f54132c5d5ca29a6ef8f43922c68e5a9bfe9bb9b
Tom Fogal <tfogal@alumni.unh.edu>
2014-04-21 20:33:30 +0200

    crazier debugging.

--- Scripts/gcc-dev-cfg.sh
+++ Scripts/gcc-dev-cfg.sh
@@ -3,7 +3,7 @@
 #-D_REENTRANT -DQT_NO_DEBUG -DQT_GUI_LIB -DQT_CORE_LIB -DQT_SHARED
 VIS="-fvisibility=hidden"
 INL="-fvisibility-inlines-hidden"
-DEBUG="-Wall -Wextra -D_DEBUG -O0 -g"
+DEBUG="-Wall -Wextra -D_DEBUG -O0 -ggdb3"
 CF="${DEBUG} -fstack-protector"
 CXF="${DEBUG} -D_GLIBCXX_CONCEPT_CHECK -fstack-protector"
 LF=""
